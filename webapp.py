de5d643bb9a6ffd0e551255b81811445fd92a994
rishu gupta <43990771+bitrish@users.noreply.github.com>
2021-09-27 16:43:30 +0530

    Added gridSearchCV to desicion trees

--- webapp.py
+++ webapp.py
@@ -364,13 +364,13 @@ def main():
 			if st.checkbox("Calculate acuuracy of the model"):
 				st.write("Accuracy of the Model:",accuracy)
 			if classifier_name=='Decision trees':
-				if st.write("Apply grid search CV to find the best parameter values for the model"):
+				if st.checkbox("Apply grid search CV to find the best parameter values for the model"):
 					param_dict={"max_depth":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}
 					grid=GridSearchCV(model,param_grid=param_dict,n_jobs=-1)
 					grid.fit(X_train,y_train)
-					grid.best_score_
+					st.write(grid.best_score_)
 					if st.write("Get the best parameter value"):
-						grid.best_params_
+						st.write(grid.best_params_)
 					
 
 			if st.checkbox("Build Confusion matrix"):
